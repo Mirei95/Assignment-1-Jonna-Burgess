beb12f0ac6bb98d079a540d80b58dc3de41ead16
Mirei95 <jonna.burgess95@gmail.com>
2025-10-29 12:45:49 +0100

    Assignment 1
    Assignment 1 YOU CAN'T DIVIDE US
    Prep course for programming 1

---
+++ scripts/scripts.js
@@ -0,0 +1,18 @@
+const foo = 0;
+const bar = 0;
+
+const mydivision = foo / bar 
+
+console.log( mydivision );
+// Expected output NaN
+
+function buttonlogclick() {
+    // The outcome will be NaN in this case
+    if ( isNaN(mydivision)) {
+        alert("You can't divide us");
+
+    } 
+
+console.log( "Divide" )
+
+}
